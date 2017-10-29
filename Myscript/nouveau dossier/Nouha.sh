dd6c74fe4b0505219d50bc94b80f6bdcc9e5fc1c
KazeKaze <33199189+KazeKaze@users.noreply.github.com>
2017-10-29 14:59:36 +0100

    Add files via upload

---
+++ Myscript/nouveau dossier/Nouha.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#
+#By Nouha KazeKaze98
+#Email    :noixstar@gmail.com
+#Facebook :https://www.facebook.com/nouha.bahouais
+#Twitter  :https://twitter.com/Nouha_bah_ouais
+#YouTube  :https://www.youtube.com/channel/UC7vG6qxGsaoduf_ZuzHWhgQ?view_as=subscriber
+#Instagram:https://www.instagram.com/nouha_bah_ouais_91/
+#Github   :https://github.com/MrRobot18b/Script-fb
+#Website  :
+# Copyright (C) 2017  Nouha-KazekazeDevop
+
+#color
+cyan='\e[0;36m'
+red='\e[1;31m'
+yellow='\e[1;33m'
+Green1='\033[1;32m'
+NC='\033[0m'
+LightGreen='\033[1;32m'
+Blue='\033[1;34m'
+Green='\033[0;32m'
+Red='\033[1;31m'
+clear
+
+
+echo -e  ${Red}" ███╗   ██╗ ██████╗ ██╗   ██╗██╗  ██╗ █████╗"
+echo -e  ${Red}" ████╗  ██║██╔═══██╗██║   ██║██║  ██║██╔══██╗"
+echo -e  ${Red}" ██╔██╗ ██║██║   ██║██║   ██║███████║███████║"
+echo -e  ${Red}" ██║╚██╗██║██║   ██║██║   ██║██╔══██║██╔══██║"
+echo -e  ${Red}" ██║ ╚████║╚██████╔╝╚██████╔╝██║  ██║██║  ██║"
+echo -e  ${Red}" ╚═╝  ╚═══╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝"
+echo -e  ${Red}"                                                                 "
+echo -e  ${Red}" ██╗  ██╗ █████╗ ███████╗███████╗██╗  ██╗ █████╗ ███████╗███████╗"
+echo -e  ${Red}" ██║ ██╔╝██╔══██╗╚══███╔╝██╔════╝██║ ██╔╝██╔══██╗╚══███╔╝██╔════╝"
+echo -e  ${Red}" █████╔╝ ███████║  ███╔╝ █████╗  █████╔╝ ███████║  ███╔╝ █████╗  "
+echo -e  ${Red}" ██╔═██╗ ██╔══██║ ███╔╝  ██╔══╝  ██╔═██╗ ██╔══██║ ███╔╝  ██╔══╝  "
+echo -e  ${Red}" ██║  ██╗██║  ██║███████╗███████╗██║  ██╗██║  ██║███████╗███████╗"
+echo -e  ${Red}" ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝"
+echo -e  ${Red}"                                                                  "
+echo -e  ${Red}" ██████╗ ███████╗██╗   ██╗ ██████╗ ██╗     ██╗   ██╗████████╗██╗ ██████╗ ███╗   ██╗"
+echo -e  ${Red}" ██╔══██╗██╔════╝██║   ██║██╔═══██╗██║     ██║   ██║╚══██╔══╝██║██╔═══██╗████╗  ██║"
+echo -e  ${Red}" ██████╔╝█████╗  ██║   ██║██║   ██║██║     ██║   ██║   ██║   ██║██║   ██║██╔██╗ ██║"
+echo -e  ${Red}" ██╔══██╗██╔══╝  ╚██╗ ██╔╝██║   ██║██║     ██║   ██║   ██║   ██║██║   ██║██║╚██╗██║"
+echo -e  ${Red}" ██║  ██║███████╗ ╚████╔╝ ╚██████╔╝███████╗╚██████╔╝   ██║   ██║╚██████╔╝██║ ╚████║"
+echo -e  ${Red}" ╚═╝  ╚═╝╚══════╝  ╚═══╝   ╚═════╝ ╚══════╝ ╚═════╝    ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝"
+
+
+echo  -e ${red}"   _____ _           _     _ _ _                               _               _       _   _     _ _       _       _ _ "
+echo  -e ${red}"  / ____( )         | |   | ( ) |                             | |             (_)     | | | |   (_) |     | |     | | |"
+echo  -e ${red}" | |    |/  ___  ___| |_  | |/| |__   ___ _   _ _ __ ___    __| |_   _  __   ___  ___ | | | |__  _| |_ ___| |__   | | |"
+echo  -e ${red}" | |____  |  __/\__ \ |_  | | | | | |  __/ |_| | | |  __/ | (_| | |_| |  \ V /| | (_) | | | |_) | | || (__| | | | |_|_|"
+echo  -e ${red}"  \_____|  \___||___/\__| |_| |_| |_|\___|\__,_|_|  \___|  \__,_|\__,_|   \_/ |_|\___/|_| |_.__/|_|\__\___|_| |_| (_|_)"
+
+
+echo -e ${yellow}" Please reply to continue name"
+read -p "What is your name" name
+echo "Hi $name , my name is Nouha "
+read -s -p "Add your password $name ?" name1      #Yourname
+read -p "Your password is: ${name1}"
+read -p "Click on enter for add your informations"
+read -p "Your Job: ${Job}"
+read -p "The name of your cat/dog: ${cat/dog}"
+read -p "The name of your bitch: ${Bitch}" bitch #YourBitch
+read -p "The name of your woman  ${woman}"       #Yourwoman
+read -p "Your adress email: ${myemail}"
+read -p "The email adress of your woman: " emailwoman          #Theemailofyourwoman
+read -p "Thank's We will contact your woman on the following email: $emailwoman and tell her that you are cheating on him $bitch"
+
+read -p "Click on enter to exit" exit
+echo "${exit}"
